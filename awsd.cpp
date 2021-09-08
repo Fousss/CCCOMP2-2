93b58d1d2e97f7259d19911dba0496c7f221fa20
Fousss <codeit00000@gmail.com>
2021-09-08 14:26:40 -0500

    adding awsd.cpp

---
+++ awsd.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(){
+    std::cout<<"* * * * * * * * "<<"\n * * * * * * * *"<<std::endl;
+    std::cout<<"* * * * * * * * "<<"\n * * * * * * * *"<<std::endl;
+    std::cout<<"* * * * * * * * "<<"\n * * * * * * * *"<<std::endl;
+    std::cout<<"* * * * * * * * "<<"\n * * * * * * * *"<<std::endl;
+
+    std::cout<<"----------------"<<std::endl;
+    
+    std::cout<<"*  *  *  *"<<"\n  *  *  *  *"<<std::endl;
+    std::cout<<"*  *  *  *"<<"\n  *  *  *  *"<<std::endl;
+    std::cout<<"*  *  *  *"<<"\n  *  *  *  *"<<std::endl;
+    std::cout<<"*  *  *  *"<<"\n  *  *  *  *"<<std::endl;
+    std::cout<<"*  *  *  *"<<"\n  *  *  *  *"<<std::endl;
+    std::cout<<"*  *  *  *"<<"\n  *  *  *  *"<<std::endl;
+    std::cout<<"*  *  *  *"<<"\n  *  *  *  *"<<std::endl;
+    std::cout<<"*  *  *  *"<<"\n  *  *  *  *"<<std::endl;
+}
