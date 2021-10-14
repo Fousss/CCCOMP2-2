d402ae89fb4060ec08ad909b554ab2c4a69acf7d
Fouss <codeit00000@gmail.com>
2021-10-14 18:13:19 -0500

    rgb

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ int main(int argc, char** argv){
             }
         }
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_SetRenderDrawColor(renderer, 255, 165, 0, 0);
         SDL_RenderClear(renderer);
 
         SDL_RenderPresent(renderer);
