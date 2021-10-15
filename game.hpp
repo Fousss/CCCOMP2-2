a38477fabe559105847106c234aa00b6fab5601f
Fouss <codeit00000@gmail.com>
2021-10-14 20:05:56 -0500

    ùpdated renderer

---
+++ game.hpp
@@ -0,0 +1,26 @@
+#ifndef game_hpp
+#define game_hpp
+#include "SDL2/SDL.h"
+#include <iostream>
+//Game logic
+class Game{
+    public:
+        Game();
+        ~Game();
+
+        void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+        
+        void handleEvents();
+        void update();
+        void render();
+        void clean();//cleans memory
+        
+        bool running(){return isRunning; } //returns bool
+
+    private:
+        int cnt=0;
+        bool isRunning;
+        SDL_Window *window;
+        SDL_Renderer *renderer;
+};
+#endif /* game_hpp */
--- main.cpp
+++ main.cpp
@@ -1,46 +1,13 @@
-#include <SDL2/SDL.h>
-#include <stdio.h>
-
-#define SCREEN_WIDTH 1280 
-#define SCREEN_HEIGHT 720
-
-int main(int argc, char** argv){
-    if(SDL_Init(SDL_INIT_VIDEO) < 0){
-        printf("Error: SDL failed to initialize\nSDL Error: '%s'\n", SDL_GetError());
-        return 1;
+#include "game.hpp"
+Game *game=nullptr;
+int main(int argc, const char * argv[]){
+    game=new Game();
+    game->init("OakEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800 , 600, false);
+    while (game->running()){    
+        game->handleEvents();
+        game->update();
+        game->render();
     }
-
-    SDL_Window *window = SDL_CreateWindow("SLD test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-    if(!window){
-        printf("Error: Failed to open window\nSDL Error: '%s'\n", SDL_GetError());
-        return 1;
-    }
-
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if(!renderer){
-        printf("Error: Failed to create renderer\nSDL Error: '%s'\n", SDL_GetError());
-        return 1;
-    }
-
-    bool running = true;
-    while(running){
-        SDL_Event event;
-        while(SDL_PollEvent(&event)){
-            switch(event.type){
-                case SDL_QUIT:
-                    running = false;
-                    break;
-
-                default:
-                    break;
-            }
-        }
-
-        SDL_SetRenderDrawColor(renderer, 255, 165, 0, 0);
-        SDL_RenderClear(renderer);
-
-        SDL_RenderPresent(renderer);
-    }
-
+    game->clean();
     return 0;
 }
